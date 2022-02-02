36baaf097680d012006567a685fc6eac01734be5
Edgar Onghena <dev@edgar.bzh>
2022-02-02 18:04:39 +0100

    Implement "Analyse des Caractères Unicode"

---
+++ Exercices/TP1/unicode/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        table {
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 1px solid black;
+            padding: 0.1em;
+        }
+
+        td > * {
+            display: block;
+            width: 100%;
+            text-align: center;
+        }
+
+        td > b {
+            font-size: 4em;
+        }
+
+        td > b > i {
+            font-size: 0.5em;
+            color: darkgray;
+        }
+    </style>
+</head>
+<body>
+    <form>
+        <label for="word">Mot: </label>
+        <input type="text" name="word" id="word" value="<?= $_GET['word'] ?: "" ?>">
+    </form>
+    <?php
+    if (isset($_GET['word'])) {
+        include 'unicode_output.php';
+    }
+    ?>
+</body>
+</html>
---
+++ Exercices/TP1/unicode/unicode_output.php
@@ -0,0 +1,57 @@
+<?php
+const UNICODE_URL = "https://util.unicode.org/UnicodeJsps/character.jsp?a=";
+
+$word = $_GET['word'];
+$word_len = mb_strlen($word);
+$first_char = mb_substr($word, 0, 1);
+
+function print_td(?string $char = null, ?int $cp = null) {
+    if (is_null($char) && !is_null($cp)) {
+        $char = mb_chr($cp);
+    } elseif (is_null($cp)) {
+        $cp = mb_ord($char);
+    } else {
+        die('missing argument');
+    }
+
+    echo '<td>';
+    $char_disp = $char;
+    if ($char_disp == " ") $char_disp = "<i>SP</i>";
+
+    echo "<b>$char_disp</b>";
+    $codepoint = str_pad(dechex($cp), 4, "0", STR_PAD_LEFT);
+    $url = UNICODE_URL . $codepoint;
+    $name = mb_substr(exec("unicode -d $cp --brief"), 2);
+
+    echo "<a href='$url' target='_blank' title='$name'>U+$codepoint</a>";
+
+    echo '</td>';
+}
+?>
+
+<table>
+    <tbody>
+        <tr>
+            <?php
+            for ($i = 0; $i < $word_len; $i++) {
+                $char = mb_substr($word, $i, 1);
+                print_td($char);
+            }
+            ?>
+        </tr>
+        <tr>
+            <th colspan="16">Sur la même ligne Unicode que le premier caractère (<?= $first_char ?>):</th>
+        </tr>
+        <tr>
+            <?php
+            $first_code = mb_ord($first_char);
+            $row_first_element = $first_code & 0b11111111111111111111111111110000;
+
+            for ($i = 0; $i < 16; $i++) {
+                $cp = $i + $row_first_element;
+                print_td(null, $cp);
+            }
+            ?>
+        </tr>
+    </tbody>
+</table>
